ab7662ebe4a6f927fc1c82b42a66bf7acb0532e3
naveenv20 <naveenv20@gmail.com>
2019-10-09 23:46:57 -0300

    baseone

---
+++ assin1/src/main/java/HibernateUtil/HibernateUtil.java
@@ -0,0 +1,27 @@
+package HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+
+public class HibernateUtil {
+
+	public Session getsession() {
+		
+		
+		// create session factory
+				SessionFactory factory = new Configuration()
+										.configure("hibernate.cfg.xml")
+										//.addAnnotatedClass(Instructor.class)
+										//.addAnnotatedClass(InstructorDetail.class)
+										.buildSessionFactory();
+				
+				// create session
+				Session session = factory.getCurrentSession();
+		return session;
+	
+	}
+	
+	
+}
