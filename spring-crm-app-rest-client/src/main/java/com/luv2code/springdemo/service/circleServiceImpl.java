bd5f79f03ea12ff3a560453d868d9c506901db9a
naveenv20 <naveenv20@gmail.com>
2019-11-01 00:33:39 -0300

    home

--- spring-crm-app-rest-client/src/main/java/com/luv2code/springdemo/service/circleServiceImpl.java
+++ spring-crm-app-rest-client/src/main/java/com/luv2code/springdemo/service/circleServiceImpl.java
@@ -1,10 +1,33 @@
 package com.luv2code.springdemo.service;
 
 import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
 
 import com.luv2code.springdemo.model.circle;
 
 public class circleServiceImpl implements circleService {
+	
+	
+	private RestTemplate restTemplate;
+
+	private String crmRestUrl;
+		
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	@Autowired
+	public circleServiceImpl(RestTemplate theRestTemplate, 
+										@Value("${crm.rest.url}") String theUrl) {
+		restTemplate = theRestTemplate;
+		crmRestUrl = theUrl;
+				
+		logger.info("Loaded property:  crm.rest.url=" + crmRestUrl);
+	}
+	
+	
 
 	@Override
 	public List<circle> getCircledata() {
