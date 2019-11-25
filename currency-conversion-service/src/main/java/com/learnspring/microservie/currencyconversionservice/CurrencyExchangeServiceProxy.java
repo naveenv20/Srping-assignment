64a59c60e3a23efc2466ee27b153a08590504090
othnvun <naveen.vundavalli@medavie.bluecross.ca>
2019-11-25 16:58:10 -0400

    ofc

--- currency-conversion-service/src/main/java/com/learnspring/microservie/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/learnspring/microservie/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -11,13 +11,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 // what micro service we want connect .. get that from app.proper spring.application.name
 //and the url for that 
 //@FeignClient(name="currency-exchange-service",url="localhost:8000")
-@FeignClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service")
+// here we are connecting to currency exchange service directly 
+
+// now use the zuul 
+
+@FeignClient(name="netflix-zuul-api-gateway-server")
 //for conversion of microservice ..we are just usng port 8000 above if we have mutiple instances then ?
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	
 	
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//after impleneting zuul
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+	
 	public CurrencyConversionBean retrieveExchangeValue
 	(@PathVariable("from") String from,@PathVariable("to") String to) ;
 		
--- currency-exchange-service/src/main/java/com/learnspring/microservie/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/learnspring/microservie/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.learnspring.microservie.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private Environment environment;
@@ -26,6 +30,7 @@ public class CurrencyExchangeController {
 		
 		// exchangeValue.setPort(8000); hard coding the port
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		logger.info("{}",exchangeValue);
 		return exchangeValue;
 	}
 
