a142c9c7c27c58a7c04743eb591525ece7333ade
othnvun <naveen.vundavalli@medavie.bluecross.ca>
2019-12-06 17:14:02 -0400

    ofc

--- step6-circle-microservice/src/main/java/com/learnspring/circle/step6circlemicroservice/controller/CircleController.java
+++ step6-circle-microservice/src/main/java/com/learnspring/circle/step6circlemicroservice/controller/CircleController.java
@@ -1,5 +1,6 @@
 package com.learnspring.circle.step6circlemicroservice.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learnspring.circle.step6circlemicroservice.entity.circle;
+import com.learnspring.circle.step6circlemicroservice.entity.circlebean;
 import com.learnspring.circle.step6circlemicroservice.service.CircleService;
 
 
@@ -45,7 +47,13 @@ public class CircleController {
 		
 		// get users from the service
 				List<circle> thecircles = circleService.findAll();
-						
+				
+				List<circlebean> thecirclesbeans= new ArrayList<>();
+				
+				for (circle e: thecircles){
+					thecirclesbeans.add(e);
+				}
+				
 				// add the customers to the model
 				theModel.addAttribute("circlelist", thecircles);
 				
--- step6-circle-microservice/src/main/java/com/learnspring/circle/step6circlemicroservice/entity/circlebean.java
+++ step6-circle-microservice/src/main/java/com/learnspring/circle/step6circlemicroservice/entity/circlebean.java
@@ -3,8 +3,29 @@ package com.learnspring.circle.step6circlemicroservice.entity;
 public class circlebean {
 	
 	private circle thecircle;
+	public circle getThecircle() {
+		return thecircle;
+	}
+	public void setThecircle(circle thecircle) {
+		this.thecircle = thecircle;
+	}
+	public user_info getTheuser_info() {
+		return theuser_info;
+	}
+	public void setTheuser_info(user_info theuser_info) {
+		this.theuser_info = theuser_info;
+	}
 	private user_info theuser_info;
+	public circlebean(circle thecircle, user_info theuser_info) {
+		super();
+		this.thecircle = thecircle;
+		this.theuser_info = theuser_info;
+	}
 	
+	
+	public circlebean() {
+		
+	}
 	
 
 }
